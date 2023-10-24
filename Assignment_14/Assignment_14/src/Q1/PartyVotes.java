0d03e12a613d83877f76397fe611796cb4765227
ManasKelkar <manaskelkar42@gmail.com>
2023-10-24 20:10:27 +0530

    Assignment_14 uploaded

---
+++ Assignment_14/Assignment_14/src/Q1/PartyVotes.java
@@ -0,0 +1,36 @@
+package Q1;
+
+public class PartyVotes {
+
+	String party;
+	int votes;
+	public PartyVotes(String party, int votes) {
+		super();
+		this.party = party;
+		this.votes = votes;
+	}
+	public PartyVotes() {
+		
+	}
+	public String getParty() {
+		return party;
+	}
+	public void setParty(String party) {
+		this.party = party;
+	}
+	public int getVotes() {
+		return votes;
+	}
+	public void setVotes(int votes) {
+		this.votes = votes;
+	}
+	@Override
+	public String toString() {
+		return "PartyVotes [party=" + party + ", votes=" + votes + "]";
+	}
+	
+	
+	
+	
+	
+}
